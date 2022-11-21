6982d120d6cddf4fea66d678c92287365c81eb50
Илья Соболев <i4sobolev@yandex.ru>
2022-11-21 18:57:51 +0300

    1

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import Comment, Review, Title
+
+
+class ReviewsAdmin(admin.ModelAdmin):
+    """Отображение модели Review в Админке."""
+
+    list_display = ('pk', 'text', 'score', 'author', 'title')
+    search_fields = ('title', 'author')
+    list_filter = ('score', 'text',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Review, ReviewsAdmin)
+admin.site.register(Title)
+admin.site.register(Comment)

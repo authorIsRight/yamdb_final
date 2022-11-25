61c3bfa2b1a25451ff1ed881f4ef97e7d9137d2d
Илья Соболев <i4sobolev@yandex.ru>
2022-11-26 01:37:15 +0300

    need for sleep

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,6 +1,10 @@
 import os
 from datetime import timedelta
 
+from dotenv import load_dotenv 
+
+load_dotenv() 
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
